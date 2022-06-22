7e415489861271ec6e4af1b11ad26357bfa3bf3c
hejiangping <2505598272@qq.com>
2022-06-22 15:05:41 +0800

    base project

---
+++ LMR/LMR/ViewController.swift
@@ -0,0 +1,14 @@
+//  Created on 2022/6/22.
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
